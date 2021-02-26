b99c1c93f225454619e198a728b820aa22316806
Nazmul Hasan <nazmulhasan747406@gmail.com>
2021-02-27 03:48:37 +0600

    make it better

--- multi_vendor/settings.py
+++ multi_vendor/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'demo@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'eastern747406@gmail.com'
+EMAIL_HOST_PASSWORD = '01766747406'
 
 
 
