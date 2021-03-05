8fb84d539c3a0c6727be9a38b715a83a9900790d
Nazmul Hasan <nazmulhasan747406@gmail.com>
2021-03-06 00:31:51 +0600

    make it better

--- multi_vendor/settings.py
+++ multi_vendor/settings.py
@@ -158,13 +158,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # smtp configuration
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
 EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
 EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
 EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
